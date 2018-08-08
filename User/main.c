4f34e8b3f33e84bf0366f6b30357ef93336bd2f3
mmmnic <mmmnic1@gmail.com>
2018-08-08 22:48:08 +0700

    add My Documents

--- User/main.c
+++ User/main.c
@@ -7,24 +7,19 @@ int main(void)
 	Init_Timer();
 	Init_Interrupt();
 	Timer_On(TIM2, 1);
-	uint8_t t=0;
 	
-//	for (t=0; t<40; t++)
-//	{
-//		InputData(1);
-//		ShiftLED();
-//	}
-//	SendData();
-//	DisplayLine(0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111);
+
+	
 	while (1)
 	{
-		if (StartPos(150))
-		{
-			DisplayWord("Hello");
-		}
-		if (StartPos(200))
-		{
-			DisplayWord("CEEC");
-		}
+		DisplayLine(0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111);
+//		if (StartPos(150))
+//		{
+//			DisplayWord("Hello");
+//		}
+//		if (StartPos(200))
+//		{
+//			DisplayWord("CEEC");
+//		}
 	}
 }
