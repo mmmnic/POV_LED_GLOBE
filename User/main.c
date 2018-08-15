8c4abd3fb9797ed4c5f077cd10b70891648f2b23
mmmnic <mmmnic1@gmail.com>
2018-08-15 15:28:24 +0700

    It's Worked

--- User/main.c
+++ User/main.c
@@ -6,21 +6,9 @@ int main(void)
 	Init_Timer();
 	Init_Interrupt();
 
-	
-//	GPIO_InitTypeDef GPIO;
-//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);	
-//	GPIO.GPIO_Pin = GPIO_Pin_13;
-//	GPIO.GPIO_Mode = GPIO_Mode_Out_PP;
-//	GPIO.GPIO_Speed = GPIO_Speed_50MHz;
-//	GPIO_Init(GPIOC, &GPIO);
-	
-//	TimingPos=0;
 	while (1)
 	{
-		if (StartPos(270))
-		{
-			DisplayChar('B');
-		}
+		DisplayWord("HELLO");
 	}
 	
 }
